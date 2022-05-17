dcefe72de027527d3b74ba55e92428e193f1036e
Jure Sunic <juresunic@hotmail.com>
2022-05-17 13:10:04 +0100

    Added a links to a navigation menu and put them in the middle in the paragraph

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
-import { images } from '../../constants';
+import { images } from '../../constants';  // Imported images from constants
 import './Navbar.scss';
 
 function Navbar() {
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar"> 
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+      <ul className="app_navbar-links">
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}> 
+            <div></div>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
