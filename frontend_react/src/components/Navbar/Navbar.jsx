8c263c35e82f3fb3bbf75ed548beb7a21197a4a8
Jure Sunic <juresunic@hotmail.com>
2022-05-17 13:59:23 +0100

    Added styling to the navigation bar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
 
 import { images } from '../../constants';  // Imported images from constants
 import './Navbar.scss';
@@ -9,14 +10,18 @@ function Navbar() {
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul className="app_navbar-links">
+      <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}> 
-            <div></div>
+            <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+
+      </div>
     </nav>
   )
 }
