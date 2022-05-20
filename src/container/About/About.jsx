1e415d7a5bdcf89e43c538c410b5cc5b4dfa239b
Jure Sunic <juresunic@hotmail.com>
2022-05-20 16:03:40 +0100

    Added High order components and started with work section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,45 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
-
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about01},
-  { title: 'Web Deisgn', description: 'I am a good web developer', imgUrl: images.about02},
-  { title: 'UI', description: 'I am a good web developer', imgUrl: images.about03},
-  { title: 'Web animations', description: 'I am a good web developer', imgUrl: images.about04}
-]
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
-      <h2 className="head-text"> 
-      I Know That 
-      <span>Good Design</span>
-      <br />
-      means 
-      <span>Good Business</span>
-      </h2>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
-          whileInView={{ opacity: 1 }}
-          whileHover={{ scale: 1.1 }}
-          transition={{ duration: 0.5, type: 'tween' }}
-          className="app__profile-item"
-          key={about.title + index}
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default About
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
-const About = () => {
+
+const Work = () => {
+
+  const [activeFilter, setactiveFilter] = useState('All');
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text">My Creative <span> Portfolio </span> section </h2>
+
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
-export default About
+export default Work
