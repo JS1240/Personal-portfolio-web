1111bc13603dff49c9f0334dd74fb3596976fe46
Jure Sunic <juresunic@hotmail.com>
2022-05-20 10:41:26 +0100

    Fixed the issues with the header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,11 +58,11 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index => (
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
-        )))}
+        ))}
       </motion.div>
     </div>
   )
