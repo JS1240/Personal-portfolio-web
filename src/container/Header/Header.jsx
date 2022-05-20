15566ffd2f86437245d5811c648f4cc776cafa76
Jure Sunic <juresunic@hotmail.com>
2022-05-20 10:34:21 +0100

    Implemented the entity of the header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -4,12 +4,23 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    trasnition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
     <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.8 }}
         className="app__header-info"
       >
         <div className="app__header-badge">
@@ -20,10 +31,38 @@ const Header = () => {
               <h1 className="head-text">Jure</h1>
             </div>
           </div>
-
+            <div classNAme="tag-cmp app__flex">
+            <p className="p-text">iOS and AI Developer</p>
+            <p className="p-text">Freelancer</p>
+            </div>
         </div>
+      </motion.div>
 
+      <motion.div
+      whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.8, delayChildren: 0.8 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
 
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        )))}
       </motion.div>
     </div>
   )
