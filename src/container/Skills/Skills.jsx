7088d372efe725bd0f36a14a578a5eb45847b65c
Jure Sunic <juresunic@hotmail.com>
2022-05-22 16:41:41 +0100

    Added a query to catch the skills and experiences from the sanity

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 import './Skills.scss';
-const About = () => {
+
+const Skills = () => {
+
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+    
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className="head-text">skills & Experience</h2>
+
+      <div className="app__skills-container">
+        <motion className="app__skills-list">
+          {/* {skills.map} */}
+        </motion>
+      </div>
+    </>
   )
 }
 
-export default About
+export default Skills
